312820ad571b6813f2b916a448c435cec80c48e8
Michal Charemza <michal@charemza.name>
2021-05-16 12:51:05 +0100

    tests: basic tests

---
+++ test.py
@@ -0,0 +1,56 @@
+import functools
+import threading
+import unittest
+
+from iterable_subprocess import iterable_subprocess
+
+
+class TestIterableSubprocess(unittest.TestCase):
+
+    def test_cat_not_necessarily_streamed(self):
+        output = b''.join(iterable_subprocess(['cat'], yield_small_input()))
+        self.assertEqual(output, b'firstsecondthird')
+
+    def test_cat_streamed(self):
+        latest_input = None
+
+        def yield_input():
+            nonlocal latest_input
+
+            for i in range(0, 10000000):
+                yield b'*' * 10
+                latest_input = i
+
+        output_chunks = iter(iterable_subprocess(['cat'], yield_input()))
+        latest_input_during_output = [latest_input for _ in output_chunks]
+
+        # Make sure the input is progressing during the output. In test, there
+        # are about 915 steps, so checking that it's greater than 50 shouldm't
+        # make this test too flakey
+        num_steps = 0
+        prev_i = 0
+        for i in latest_input_during_output:
+            if i != prev_i:
+                num_steps += 1
+            prev_i = i
+        self.assertGreater(num_steps, 50)
+
+    def test_exception_from_input_propagated(self):
+        def yield_input():
+            raise Exception('Something went wrong')
+
+        with self.assertRaisesRegex(Exception, 'Something went wrong'):
+            b''.join(iterable_subprocess(['cat'], yield_input()))
+
+    def test_exception_from_not_found_process_propagated(self):
+        def yield_input():
+            raise Exception('Something went wrong')
+
+        with self.assertRaises(FileNotFoundError):
+            b''.join(iterable_subprocess(['does-not-exist'], yield_small_input()))
+
+
+def yield_small_input():
+    yield b'first'
+    yield b'second'
+    yield b'third'
